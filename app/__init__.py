bfb7600252fe5806aeb3b2219200d7b107643f67
Poochy2017 <hnn2cdzr82@privaterelay.appleid.com>
2026-07-11 20:43:52 -0400

    Refactor app initialization and add CORS support
    Refactored app initialization to use create_app function and added CORS support.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,18 @@
-# app/__init__.py
+from flask import Flask
+from flask_cors import CORS
+
+def create_app():
+    app = Flask(__name__, template_folder="templates")
+    CORS(app)
+    
+    from .routes import bp
+    app.register_blueprint(bp)
+    
+    return app
+
+
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
