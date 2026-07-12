c459ce93899fede58442b0565fcc0f2bfb594d6d
Poochy2017 <hnn2cdzr82@privaterelay.appleid.com>
2026-07-11 20:51:53 -0400

    Refactor __init__.py to clean up app initialization
    Removed unnecessary route definitions and Flask app initialization from __init__.py.

--- app/__init__.py
+++ app/__init__.py
@@ -9,26 +9,3 @@ def create_app():
     app.register_blueprint(bp)
     
     return app
-
-
-
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/upload')
-def upload():
-    return render_template('upload.html')
-
-@app.route('/play')
-def play():
-    return render_template('play.html')
-
-@app.route('/results')
-def results():
-    return render_template('results.html')
